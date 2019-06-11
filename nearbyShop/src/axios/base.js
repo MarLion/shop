b12d3d7ae0795161afc2a54f3ebf00a2b3e07fb5
sheyu <925223337@qq.com>
2019-06-11 10:00:25 +0800

    修改环境

--- nearbyShop/src/axios/base.js
+++ nearbyShop/src/axios/base.js
@@ -1,5 +1,5 @@
 export const baseUrl = {
   //serviceOne:'http://192.168.1.198:6203',//测试服 198
   serviceOne:'http://192.168.1.198:62033',//内网 预上线地址
-  //serviceOne:'http://yys.zhongwei-info.com/business-shop-service',//正式
+  //serviceOne:'',//正式
 };
